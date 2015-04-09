d60e5d184a1b18cfaf64da8bc3218e3b8cbb8c56
GulliverJ <gully.johnson@gmail.com>
2015-04-10 00:06:47 +0100

    Added minor set-up changes to the analytics engine

--- orange-parking-analytics/src/main/java/main/AnalyticsRunner.java
+++ orange-parking-analytics/src/main/java/main/AnalyticsRunner.java
@@ -2,14 +2,16 @@ package main;
 
 import com.orange.api.session.*;
 
+
 public class AnalyticsRunner {
 
+	// This main function will instantiate the threads
 	public static void main(String[] args) {
 		demoAPI();
 
-		
 	}
 	
+	// Returns
 	private static void demoAPI() {
 		OrangeConnection api = new OrangeConnection();
 		int[] allParkingSensors = api.getIDsWhere("application", "Orange Parking");
